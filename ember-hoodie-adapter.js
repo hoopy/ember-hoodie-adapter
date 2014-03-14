ba48a07e762ae3bd9e96b8e9f6251c584bd88044
Gregor Martynus <gregor@martynus.net>
2014-03-14 12:07:56 +0100

    [fix] make promises returned by Hoodie Promises/A+ compliant
    fixes #1

--- ember-hoodie-adapter.js
+++ ember-hoodie-adapter.js
@@ -4,26 +4,41 @@
 (function () {
   'use strict';
 
+  function wrapJqueryPromise(jQueryPromise) {
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      jQueryPromise.then(function(json) {
+        Ember.run(null, resolve, json);
+      }, function(error) {
+        Ember.run(null, reject, error);
+      });
+    });
+  }
+
   DS.HoodieAdapter = DS.Adapter.extend(Ember.Evented, {
 
     find: function(store, type, id/*, opts*/) {
-      return hoodie.store.find(type.typeKey, id);
+      return wrapJqueryPromise( hoodie.store.find(type.typeKey, id) );
+
     },
 
     findAll: function (store, type) {
-      return hoodie.store.findAll(type.typeKey);
+      return wrapJqueryPromise( hoodie.store.findAll(type.typeKey) );
+
     },
 
     createRecord: function (store, type, record) {
-      return hoodie.store.add(type.typeKey, record);
+      return wrapJqueryPromise( hoodie.store.add(type.typeKey, record) );
+
     },
 
     updateRecord: function (store, type, record) {
-      return hoodie.store.update(type.typeKey, record.id);
+      return wrapJqueryPromise( hoodie.store.update(type.typeKey, record.id) );
+
     },
 
     deleteRecord: function (store, type, record) {
-      return hoodie.store.remove(type.typeKey, record.id);
+      return wrapJqueryPromise( hoodie.store.remove(type.typeKey, record.id) );
+
     }
   });
 }());
